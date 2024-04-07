95ccb1808d4869927b597cd0ba34aedfd7203c3a
lvanjelg <lukevanjelg@gmail.com>
2024-04-07 15:39:16 -0400

    .

--- src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/model/PancakeHouseIterator.java
+++ src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/model/PancakeHouseIterator.java
@@ -25,4 +25,16 @@ public class PancakeHouseIterator implements Iterator {
         position = position + 1;
         return menuItem;
     }
+    @Override
+    public void remove(){
+        if(position <= 0){
+            throw new IllegalStateException("You can't remove an item until you've done at least one next()");
+        }
+        if(items.get(position-1)!=null){
+            for(int i = position-1; i < (items.size()-1); i++){
+                items.set(i,items.get(i+1));
+            }
+            items.set(items.size()-1,null);
+        }
+    }
 }
